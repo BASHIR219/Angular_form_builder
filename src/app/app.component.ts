f2e32de77033a89e442987d5af8892f74109a25f
Vishal Kumar <vishalkumar@Vishals-MacBook-Air.local>
2025-01-16 18:30:01 +0530

    Added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { FormBuilderComponent } from './form-builder/form-builder.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormBuilderComponent,RouterModule],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
